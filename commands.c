703c421e650bf7aa201a0a8dcb0c9322d7b47a2c
Tomasz Miśków <mysquff@gmail.com>
2017-03-26 19:35:02 +0200

    Added DELETE_NODE command.

--- commands.c
+++ commands.c
@@ -6,17 +6,24 @@
 
 void commandAddNode(Tree*, int);
 void commandRightmostChild(Tree*, int);
+void commandDeleteNode(Tree*, int);
 
 /* COMMAND FUNCTIONS DEFINITIONS */
 
 // ADD_NODE
 void commandAddNode(Tree* tree, int parentIndex) {
 	addTreeNode(tree, parentIndex);
-	printf("OK\n");
+	//printf("OK\n");
 }
 
 // RIGHTMOST_CHILD
 void commandRightmostChild(Tree* tree, int parentIndex) {
-	int rightmostChildIndex = getRightmostChildIndex(tree, parentIndex);
+	int rightmostChildIndex = getIndexOfRightmostChild(tree, parentIndex);
 	printf("%d\n", rightmostChildIndex);
+}
+
+// DELETE_NODE
+void commandDeleteNode(Tree* tree, int index) {
+	deleteTreeNodeByIndex(tree, index);
+	//printf("OK\n");
 }
--- commands.h
+++ commands.h
@@ -4,5 +4,7 @@
 // ADD_NODE
 extern void commandAddNode(Tree*, int);
 extern void commandRightmostChild(Tree*, int);
+extern void commandDeleteNode(Tree*, int);
+
 
 #endif /* COMMANDS_H */
