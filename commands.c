5710dd2e6730697a7a7cbf50cadef8a2b4801912
Tomasz Miśków <mysquff@gmail.com>
2017-03-26 23:45:47 +0200

    Added DELETE_NODE command.

--- commands.c
+++ commands.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
+#include <string.h>
 
 #include "tree.h"
+#include "commands.h"
 
 /* COMMAND FUNCTIONS DECLARATIONS */
+
+Command selectCommand(char*);
 
 void commandAddNode(Tree*, int);
 void commandRightmostChild(Tree*, int);
@@ -10,10 +14,44 @@ void commandDeleteNode(Tree*, int);
 
 /* COMMAND FUNCTIONS DEFINITIONS */
 
+// Selects command based on string.
+Command selectCommand(char* string) {
+
+	// ADD_NODE
+	if (strcmp(string, "ADD_NODE") == 0) {
+		return COMMAND_ADD_NODE;
+	}
+
+	// RIGHTMOST_CHILD
+	else if (strcmp(string, "RIGHTMOST_CHILD") == 0) {
+		return COMMAND_RIGHTMOST_CHILD;
+	}
+
+	// DELETE_NODE
+	else if (strcmp(string, "DELETE_NODE") == 0) {
+		return COMMAND_DELETE_NODE;
+	}
+
+	// DELETE_SUBTREE
+	else if (strcmp(string, "DELETE_SUBTREE") == 0) {
+		return COMMAND_DELETE_SUBTREE;
+	}
+
+	// SPLIT_NODE
+	else if (strcmp(string, "SPLIT_NODE") == 0) {
+		return COMMAND_SPLIT_NODE;
+	}
+
+	// Unknown command.
+	else {
+		return COMMAND_UNKNOWN;
+	}
+
+}
+
 // ADD_NODE
 void commandAddNode(Tree* tree, int parentIndex) {
 	addTreeNode(tree, parentIndex);
-	//printf("OK\n");
 }
 
 // RIGHTMOST_CHILD
@@ -25,5 +63,4 @@ void commandRightmostChild(Tree* tree, int parentIndex) {
 // DELETE_NODE
 void commandDeleteNode(Tree* tree, int index) {
 	deleteTreeNodeByIndex(tree, index);
-	//printf("OK\n");
 }
--- commands.h
+++ commands.h
@@ -1,9 +1,29 @@
 #ifndef COMMANDS_H
 #define COMMANDS_H
 
+
+enum Command;
+
+typedef enum Command Command;
+
+enum Command {
+	COMMAND_ADD_NODE,
+	COMMAND_RIGHTMOST_CHILD,
+	COMMAND_DELETE_NODE,
+	COMMAND_DELETE_SUBTREE,
+	COMMAND_SPLIT_NODE,
+	COMMAND_UNKNOWN
+};
+
+extern Command selectCommand(char*);
+
 // ADD_NODE
 extern void commandAddNode(Tree*, int);
+
+// RIGHTMOST_CHILD
 extern void commandRightmostChild(Tree*, int);
+
+// DELETE_NODE
 extern void commandDeleteNode(Tree*, int);
 
 
