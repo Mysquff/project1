6d1bcf40dcdf3a8a6acfd85e06a2fae0ff6cbf45
Tomasz Miśków <mysquff@gmail.com>
2017-03-26 16:44:20 +0200

    Got rid of struct List. Implemented functions neccessary to ADD_NODE command and those used in freeing memory.

--- list.c
+++ list.c
@@ -1,38 +1,27 @@
-#include "stdlib.h"
+#include <stdlib.h>
 
-// Node type used in both trees and lists.
-struct Node;
-typedef struct Node Node;
-
-// Double-linked list type.
-struct List;
-typedef struct List List;
+#include "list.h"
 
 struct Node {
 	int index;
 
-	List* childrenList;
-
 	// Pointers to neighbours.
 	Node* left;
 	Node* right;
-};
 
-struct List {
-	Node* leftSentinel;
-	Node* rightSentinel;
+	// Pointers to sons
+	Node* leftmostChild;
+	Node* rightmostChild;
+
+	// Used only in either leftmost or rightmost sons. Otherwise set to null.
+	Node* parent;
 };
 
 /* FUNCTION DECLARATIONS */
 void initializeNode(Node**, int);
-void initializeSentinel(Node**);
+void destroyNode(Node*);
 void linkNodes(Node*, Node*);
-void addLeftNode(Node*, Node*);
-void addRightNode(Node*, Node*);
 void AddNodeToParent(Node*, Node*);
-
-void initializeList(List**);
-void addNodeToList(List*, Node*);
 
 /* FUNCTION DEFINITIONS */
 
@@ -47,81 +36,81 @@ void initializeNode(Node** nodePointer, int index) {
 	// Set node's index.
 	node->index = index;
 
-	// Set neighbours' pointers to NULL.
+	// Initialize pointers to neighbours,
 	node->left = NULL;
 	node->right = NULL;
 
-	// Initialzie children list.
-	initializeList(&(node->childrenList));
+	// to leftmost and rightmost children,
+	node->leftmostChild = NULL;
+	node->rightmostChild = NULL;
+
+	// and a parent.
+	node->parent = NULL;
 }
-
-// Initializes list sentinel.
-void initializeSentinel(Node** sentinelPointer) {
-	// Allocate sentinel.
-	(*sentinelPointer) = (Node*) malloc(sizeof (Node));
-
-	// Temporary pointer.
-	Node* sentinel = (*sentinelPointer);
-
-	// Set index to -1.
-	sentinel->index = -1; // -1 is restricted only to sentinels.
-
-	// Set  neighbours' pointers to NULL.
-	sentinel->left = NULL;
-	sentinel->right = NULL;
-
-	// Sentinels do not have children, so set children list to NULL.
-	sentinel->childrenList = NULL;
+// Destroys a node and frees its memory.
+void destroyNode(Node* node) {
+	free(node);
 }
 
 // Links two nodes. First one goes to the left, second one to the right.
 void linkNodes(Node* leftNode, Node* rightNode) {
-	leftNode->right = rightNode;
-	rightNode->left = leftNode;
-}
 
-// Puts new node to the left of right node.
-void addLeftNode(Node* rightNode, Node* newNode) {
+	// Both nodes exist.
+	if (leftNode && rightNode) {
+		leftNode->right = rightNode;
+		rightNode->left = leftNode;
+	}
 
-	// Temporary pointer to original left neighbour of the right node.
-	Node* leftNode = rightNode->left;
+	// Only left node exists.
+	else if (leftNode) {
+		leftNode->right = NULL;
+	}
 
-	// Place new node between left node and right node.
-	linkNodes(leftNode, newNode);
-	linkNodes(newNode, rightNode);
-}
-
-// Puts new node to the right of left node.
-void addRightNode(Node* leftNode, Node* newNode) {
-
-	// Temporary pointer to original left neighbour of the right node.
-	Node* rightNode = leftNode->right;
-
-	// Place new node between left node and right node.
-	linkNodes(leftNode, newNode);
-	linkNodes(newNode, rightNode);
+	// Only right node exists.
+	else {
+		rightNode->left = NULL;
+	}
 }
 
 // Adds new child to parent
 void AddNodeToParent(Node* parent, Node* child) {
-	addNodeToList(parent->childrenList, child);
-}
 
-// Initializes list.
-void initializeList(List** listPointer) {
-	// Allocate list.
-	(*listPointer) = (List*) malloc(sizeof (List));
+	// Parent node does not have children yet.
+	if (parent->leftmostChild == NULL && parent->rightmostChild == NULL) {
+		// Link a parent to a child,
+		parent->leftmostChild = child;
+		parent->rightmostChild = child;
 
-	// Temporary pointer.
-	List* list = (*listPointer);
+		// and a child to a parent.
+		child->parent = parent;
+	}
 
-	// Initialize both sentinels and link them.
-	initializeSentinel(&(list->leftSentinel));
-	initializeSentinel(&(list->rightSentinel));
-	linkNodes(list->leftSentinel, list->rightSentinel);
-}
+	// Parent has only one child.
+	else if (parent->leftmostChild == parent->rightmostChild) {
+		// Place a new child as a new rightmostChild.
+		linkNodes(parent->rightmostChild, child);
 
-// Puts node on the right end of the list, just before the right sentinel.
-void addNodeToList(List* list, Node* node) {
-	addLeftNode(list->rightSentinel, node);
+		// Link parent to the new child.
+		parent->rightmostChild = child;
+
+		// Link new child to parent.
+		child->parent = parent;
+	}
+
+
+	// Parent has one or more children.
+	else {
+		// Place a new child as a new rightmostChild.
+		linkNodes(parent->rightmostChild, child);
+
+		// Set parent pointer of the old rightmostChild to NULL.
+		(parent->rightmostChild)->parent = NULL;
+
+		// Link parent to the new child.
+		parent->rightmostChild = child;
+
+		// Link new child to parent.
+		child->parent = parent;
+	}
+
 }
--- list.h
+++ list.h
@@ -1,21 +1,13 @@
 #ifndef LIST_H
 #define LIST_H
 
+// Node type used in both trees and lists.
 struct Node;
 typedef struct Node Node;
 
-struct List;
-typedef struct List List;
-
 extern void initializeNode(Node**, int);
-extern void initializeSentinel(Node**);
+extern void destroyNode(Node*); 
 extern void linkNodes(Node*, Node*);
-extern void addLeftNode(Node*, Node*);
-extern void addRightNode(Node*, Node*);
 extern void AddNodeToParent(Node*, Node*);
-
-
-extern void initializeList(List**);
-extern void addNodeToList(List*, Node*);
 
 #endif /* LIST_H */
