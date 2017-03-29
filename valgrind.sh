2743933deab8d4050878e1f916fd02d9bbdaf1d7
Tomasz Miśków <mysquff@gmail.com>
2017-03-29 17:17:29 +0200

    Hid valgrind output.

--- valgrind.sh
+++ valgrind.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
-make debug
-valgrind --error-exitcode=15 --leak-check=full --show-leak-kinds=all --errors-for-leak-kinds=all ./solution.gdb
+
+for f in tests/*.in; do
+	valgrind > /dev/null --error-exitcode=15 --leak-check=full --show-leak-kinds=all --errors-for-leak-kinds=all ./solution.gdb > /dev/null < $f
+	if [ "$?" = "15" ]; then
+		echo $f
+	fi
+done
