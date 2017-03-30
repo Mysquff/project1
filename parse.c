7b9e714515f7f7cb5093990d946a5f5431d337b0
Tomasz Miśków <mysquff@gmail.com>
2017-03-30 12:31:47 +0200

    Added string specifier macro. Made small style fixes.

--- parse.c
+++ parse.c
@@ -5,14 +5,19 @@
 #include "tree.h"
 #include "commands.h"
 
+// Maximum length of command string.
 #define MAX_COMMAND_NAME_LENGTH 16
 
+// String specifier used in scanf().
+#define INPUT_STRING_SPECIFIER "%16s"
+
+// Parses input in the loop and runs selected commands.
 void parseInput(Tree* tree, bool isDebugMode) {
 
 	char buffer[MAX_COMMAND_NAME_LENGTH];
 
 	// Run while there is input.
-	while (scanf("%s", buffer) != EOF) {
+	while (scanf(INPUT_STRING_SPECIFIER, buffer) != EOF) {
 
 		int parameter1, parameter2;
 
@@ -22,29 +27,34 @@ void parseInput(Tree* tree, bool isDebugMode) {
 		// Run selected command.
 		switch (command) {
 
+			// ADD_NODE
 			case COMMAND_ADD_NODE:
 				scanf("%d", &parameter1);
 				commandAddNode(tree, parameter1);
 				printf("OK\n");
 			break;
 
+			// RIGHTMOST_CHILD
 			case COMMAND_RIGHTMOST_CHILD:
 				scanf("%d", &parameter1);
 				commandRightmostChild(tree, parameter1);
 			break;
 
+			// DELETE_NODE
 			case COMMAND_DELETE_NODE:
 				scanf("%d", &parameter1);
 				commandDeleteNode(tree, parameter1);
 				printf("OK\n");
 			break;
 
+			// DELETE_SUBTREE
 			case COMMAND_DELETE_SUBTREE:
 				scanf("%d", &parameter1);
 				commandDeleteSubtree(tree, parameter1);
 				printf("OK\n");
 			break;
 
+			// SPLIT_NODE
 			case COMMAND_SPLIT_NODE:
 				scanf("%d %d", &parameter1, &parameter2);
 				commandSplitNode(tree, parameter1, parameter2);
